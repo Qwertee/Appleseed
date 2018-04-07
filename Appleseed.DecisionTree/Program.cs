ac6c2b6ab5d12ea54f10af3d97bad1e4357a12dd
qwertee <mobiusdragon96@gmail.com>
2018-04-07 09:52:23 -0500

    initial commit

---
+++ Appleseed.DecisionTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appleseed.DecisionTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
